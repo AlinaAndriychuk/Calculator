698d5aa92498b8983d31bb07d6506bfe51ad3e5d
AlinaAndriychuk <alinka.andriychuk2002@gmail.com>
2020-12-04 17:29:15 +0200

    build project

---
+++ public/build/precache-manifest.a6ab5af222b23955a508a5c388d022a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "747912bb6299eaadd9cd75a98be54c11",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f6fdffb0a2ce93d6b46a",
+    "url": "/static/css/main.40aeea98.chunk.css"
+  },
+  {
+    "revision": "505b5979d537c8b12315",
+    "url": "/static/js/2.41e86974.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.41e86974.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6fdffb0a2ce93d6b46a",
+    "url": "/static/js/main.5b984ab3.chunk.js"
+  },
+  {
+    "revision": "797afd30eb378f627c61",
+    "url": "/static/js/runtime-main.d7d96907.js"
+  }
+]);
